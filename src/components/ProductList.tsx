4361c246907ff52581f478687a9a69da3b2e1235
Paolo Flores <maximo_150@hotmail.com>
2024-10-29 11:53:31 -0500

    Display products in a grid

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react"
 import { useProductStore } from "../store"
+import { AgGridReact } from 'ag-grid-react'
+import { ColDef } from "ag-grid-community"
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
 
 export default function ProductList() {
   const { products, fetchProducts } = useProductStore()
@@ -8,18 +12,23 @@ export default function ProductList() {
     fetchProducts()
   }, [fetchProducts])
 
+  const columns: ColDef[] = [
+    { headerName: 'ID', field: 'id' },
+    { headerName: 'Title', field: 'title' },
+    { headerName: 'Description', field: 'description', flex: 1 },
+    { headerName: 'Price', field: 'price' }
+  ]
+
   return (
-    <>
+    <div className="ag-theme-alpine" style={{ height: 450, width: '100%' }}>
       <h1 className="text-3xl text-center text-violet-600 font-bold mb-4">Lista de productos Belcorp</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <p>Precio: ${product.price}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+      <AgGridReact
+        rowData={products}
+        columnDefs={columns}
+        pagination={true}
+        paginationPageSize={10}
+        paginationPageSizeSelector={[10, 15, 20]}
+      />
+    </div>
   )
 }
