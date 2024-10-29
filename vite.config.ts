9d2093d900f0aa882a3a11f607d99a89bab2ea48
Paolo Flores <maximo_150@hotmail.com>
2024-10-29 15:34:07 -0500

    Ajustar el archivo vite.config.ts y .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Dividir dependencias en chunks separados
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
