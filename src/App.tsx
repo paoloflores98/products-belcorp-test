7819eda8352471c70e604491adb36b8c2a811248
Paolo Flores <maximo_150@hotmail.com>
2024-10-29 10:53:44 -0500

    Install Radix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button, Text, Flex } from "@radix-ui/themes"
+
 function App() {
   
   return (
     <>
-      <h1 className="text-3xl">Hola mundo</h1>
+      <h1 className="text-3xl mb-4">Hola mundo</h1>
+      <Flex direction="row" gap="2">
+        <Text>Hola mundo desde Radix UI</Text>
+        <Button size="2" variant="soft">Vamos</Button>
+      </Flex>
     </>
   )
 }
